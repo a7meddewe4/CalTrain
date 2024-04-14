dfd7d63d64023d4cfaaba138a9c732d251410b0b
a7meddewe4 <121269309+a7meddewe4@users.noreply.github.com>
2024-04-14 14:38:09 +0200

    Create caltrain.c

---
+++ caltrain.c
@@ -0,0 +1,76 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "caltrain.h"
+
+
+void station_init(struct station *station)
+{
+	
+    station->numberOfEmptySeats=0;
+    station->numberOfPassengersinTheStation=0;
+    station->numberOfPassengersWalkinginTheTrain=0;
+     pthread_mutex_init(&(station->mutex), NULL);
+    pthread_cond_init(&(station->TrainWaiting), NULL);
+    pthread_cond_init(&(station->Traincame), NULL);
+    
+}
+
+void station_load_train(struct station *station, int count)
+{
+
+    pthread_mutex_lock(&(station->mutex));
+    //printf("train came  now %d\n",count);
+	station->numberOfEmptySeats=count;
+    if(station->numberOfPassengersinTheStation>0)pthread_cond_broadcast(&(station->Traincame));
+    while((station->numberOfEmptySeats!=0)&&(station->numberOfPassengersinTheStation!=0))
+    {
+        pthread_cond_wait(&(station->TrainWaiting),&(station->mutex));
+    }
+
+  //  printf("train left  empty :%d  %d  %d\n",station->numberOfEmptySeats,station->numberOfPassengersWalkinginTheTrain , station->numberOfPassengersinTheStation);
+    station->numberOfEmptySeats=0;
+    station->numberOfPassengersWalkinginTheTrain=0;
+    pthread_mutex_unlock(&(station->mutex));
+   
+}
+
+void station_wait_for_train(struct station *station)
+{
+    
+    pthread_mutex_lock(&(station->mutex));
+    //printf("i am passenger \n");
+	station->numberOfPassengersinTheStation++;
+     
+    while(station->numberOfEmptySeats==station->numberOfPassengersWalkinginTheTrain)
+    {
+         
+         pthread_cond_wait(&(station->Traincame),&(station->mutex));    
+    }
+   // printf("i found a %d chairs we are %d person and %d standing in the train\n",station->numberOfEmptySeats, station->numberOfPassengersinTheStation,station->numberOfPassengersWalkinginTheTrain);
+    //wait untill a train with at least 1 empty chair
+    
+    station->numberOfPassengersinTheStation--;
+    station->numberOfPassengersWalkinginTheTrain++;
+    
+   
+    
+    pthread_mutex_unlock(&(station->mutex));
+}
+
+void station_on_board(struct station *station)
+{
+       
+    pthread_mutex_lock(&(station->mutex));
+    station->numberOfPassengersWalkinginTheTrain--;
+         station->numberOfEmptySeats--;
+     if(station->numberOfPassengersWalkinginTheTrain==0||station->numberOfEmptySeats==0)
+    {
+        pthread_cond_signal(&(station->TrainWaiting));
+    }
+    
+   
+    pthread_mutex_unlock(&(station->mutex));
+    
+
+}
