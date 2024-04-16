0936839f3337b70ab3e8e0c5d6b3a9158c248215
ahmed dewedar <121269309+a7meddewe4@users.noreply.github.com>
2024-04-16 03:37:58 +0200

    Update repeat.sh

--- repeat.sh
+++ repeat.sh
@@ -1,5 +1,5 @@
 for i in {1..1000}
 do
   echo "Test(" $i ") ................................................. :-" >> final_result.out
-  ./a.out >> final_result.out
+  ./caltrain >> final_result.out
 done
